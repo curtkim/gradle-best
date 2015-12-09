2f4b5854a37005c70db751a6ca0d79e6b6ba8153
curt-kim <curt.kim@daumkakao.com>
2015-12-09 12:07:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 목적
+- groovy test
+- groovy integration test
+- subproject java
+- main project use subproject jar
+- git clone으로 실행가능
+- 여러개의 main을 bin에서 제어
+
+
+# Test
+
+    gradle test -i
---
+++ src/test/groovy/PersonTest.groovy
@@ -0,0 +1,13 @@
+import org.junit.Test
+
+import static groovy.test.GroovyAssert.shouldFail
+
+class PersonTest {
+
+  @Test
+  void create() {
+    def p = new Person('abc')
+    assert p.name == 'abc1'
+  }
+
+}
